1a97140d7bd8021d5150e724b584567a87911440
Haris Aftab <100057357+Harrisaftab@users.noreply.github.com>
2025-04-16 16:42:27 +0500

    ds

--- resources/views/include/frontend/header.blade.php
+++ resources/views/include/frontend/header.blade.php
@@ -60,7 +60,7 @@
         <div class="hpt-header-2-wrap">
             <div class="container h1-container">
                 <div class="hpt-header-2-content">
-                    <a class="hpt-header-2-logo" href="home-2.html">
+                    <a class="hpt-header-2-logo" href="{{ route('home') }}">
                         <img src="public/assets/img/new_home/logo/Logo-color-2.png" alt="logo">
                     </a>
                     <nav class="hpt-header-2-menu" id="mobile_menu">
