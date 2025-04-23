fdd89a94fa6a7aa1bab1eda17f880f8ab6f8ec02
Haris Aftab <100057357+Harrisaftab@users.noreply.github.com>
2025-04-24 03:23:35 +0500

    gh

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\PageController;
+ 
+ Route::get('/', [PageController::class, 'home'])->name('home');
+ Route::get('/about', [PageController::class, 'about'])->name('about');
+ Route::get('/services', [PageController::class, 'services'])->name('services');
+ Route::get('/blog', [PageController::class, 'blog'])->name('blog');
+ Route::get('/contact', [PageController::class, 'contact'])->name('contact');
+ Route::get('/car-quote', [PageController::class, 'carQuote'])->name('car.quote');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
