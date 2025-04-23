b7aa96ef0e93027c1554422b2337441193fc86a5
Haris Aftab <100057357+Harrisaftab@users.noreply.github.com>
2025-04-24 04:06:49 +0500

    ads

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,7 +10,22 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <link rel="shortcut icon" href="public/assets/images/favicon.ico">
+    
+        <!-- Fonts css load -->
+        <link rel="preconnect" href="https://fonts.googleapis.com/">
+        <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+        <link id="fontsLink" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&amp;display=swap" rel="stylesheet">
+        <!-- Layout config Js -->
+        <script src="public/assets/js/layout.js"></script>
+        <!-- Bootstrap Css -->
+        <link href="public/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <!-- Icons Css -->
+        <link href="public/assets/css/icons.min.css" rel="stylesheet" type="text/css">
+        <!-- App Css-->
+        <link href="public/assets/css/app.min.css" rel="stylesheet" type="text/css">
+        <!-- custom Css-->
+        <link href="public/assets/css/custom.min.css" rel="stylesheet" type="text/css">
         <!-- Scripts -->
         {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
     </head>
@@ -26,5 +41,9 @@
                 {{ $slot }}
             </div>
         </div>
+            <!-- JAVASCRIPT -->
+    <script src="public/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="public/assets/libs/simplebar/dist/simplebar.min.js"></script>
+    <script src="public/assets/js/plugins.js"></script>    <script src="public/assets/js/pages/password-addon.init.js"></script>
     </body>
 </html>
