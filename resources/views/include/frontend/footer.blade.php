b2f8585569ebbd8beb1642eba20ff8eecbed77d4
Haris Aftab <100057357+Harrisaftab@users.noreply.github.com>
2025-05-15 04:40:18 +0500

    dsa

--- resources/views/include/frontend/footer.blade.php
+++ resources/views/include/frontend/footer.blade.php
@@ -91,7 +91,7 @@
 			<div class="container">
 				<div class="bi-footer-logo">
 					<div class="brand-logo text-center">
-						<a href="#"><img src="#" alt="State-To-State-Auto-Transport-Logo"></a>
+						<a href="{{ route('home') }}"><img src="public/assets/img/logo/NEW_LOGO_BG.png" alt="State-To-State-Auto-Transport-Logo"></a>
 					</div>
 					<div class="logo-mail text-center headline">
 						<h3><a href="#">contact@statetostateautotransportllc.com</a></h3>
